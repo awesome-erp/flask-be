2563ab3e5366a2993fba8afb3d82253f6b50b28c
LoopThrough-i-j <dganguly1120@gmail.com>
2021-01-04 11:42:17 +0530

    added lint and setup script

---
+++ setup/set-env.sh
@@ -0,0 +1,3 @@
+virtualenv -p python3 venv-awesome-erp
+source venv-awesome-erp/bin/activate
+pip3 install -r requirements.txt
---
+++ setup/set-linter.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/zulip/zulint.git
+cd zulint
+python3 setup.py install
+sudo rm -r ../zulint
