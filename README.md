427d77217e8d12102009e6fe2a03f8806ab34927
LoopThrough-i-j <dganguly1120@gmail.com>
2021-01-20 17:55:30 +0530

    Setup build files for heroku

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # flask-be
 
 This Repo is still under setup and Progress
+
+deployed at: https://awesome-erp-backend.herokuapp.com/
--- aerp/database/__init__.py
+++ aerp/database/__init__.py
@@ -1,8 +1,12 @@
 from firebase_admin import firestore, credentials, initialize_app
+import os
+import json
 
-db_credentials = credentials.Certificate("config/firebase_server.json")
-firebase_default_app = initialize_app(db_credentials, {
-    'storageBucket': 'anc-ang-dev-firebase.appspot.com'
-})
+if os.environ["AERP_FIREBASE_CONFIG"]:
+    creds_dict = json.loads(os.environ["AERP_FIREBASE_CONFIG"])
+    db_credentials = credentials.Certificate(creds_dict)
+else:
+    db_credentials = credentials.Certificate("config/firebase_server.json")
 
+firebase_default_app = initialize_app(db_credentials)
 database = firestore.client(firebase_default_app)
