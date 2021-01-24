7036e90ec9c5d801ce1de9d1faf80f96b949b211
LoopThrough-i-j <dganguly1120@gmail.com>
2021-01-24 17:44:25 +0530

    Bug: Fix manager update.

--- aerp/utils/responses.py
+++ aerp/utils/responses.py
@@ -11,6 +11,7 @@ def success(key: str = "", value: Any = "", code: int = 200) -> wrappers.Respons
 def failure(key: str = "", value: Any = "", code: int = 401) -> wrappers.Response:
 
     responseMessages = {401: "ID token verification failed",
+                        400: "Internal Error",
                         500: "Internal Server Error"}
     responseDict = {"status": "Failure",
                     "message": responseMessages[code]}
